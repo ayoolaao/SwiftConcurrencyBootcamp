a35628633c5c66e21ea32a555c2213d67001c8d1
Ayoola Abimbola <ao.abimbola@gmail.com>
2026-03-23 06:03:36 -0500

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Ayoola Abimbola on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
