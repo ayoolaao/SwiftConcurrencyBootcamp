40e865c1c5dd35905f7174b2ce01ae4b4ed515b3
Ayoola Abimbola <ao.abimbola@gmail.com>
2026-03-29 02:33:06 -0500

    Task lesson

--- SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -1,17 +1,18 @@
-//
-//  SwiftConcurrencyBootcampApp.swift
-//  SwiftConcurrencyBootcamp
-//
-//  Created by Ayoola Abimbola on 3/23/26.
-//
+  //
+  //  SwiftConcurrencyBootcampApp.swift
+  //  SwiftConcurrencyBootcamp
+  //
+  //  Created by Ayoola Abimbola on 3/23/26.
+  //
 
 import SwiftUI
 
 @main
 struct SwiftConcurrencyBootcampApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+            TaskBootcamp()
     }
+  }
 }
--- SwiftConcurrencyBootcamp/TaskBootcamp.swift
+++ SwiftConcurrencyBootcamp/TaskBootcamp.swift
@@ -1,18 +1,79 @@
-//
-//  TaskBootcamp.swift
-//  SwiftConcurrencyBootcamp
-//
-//  Created by Ayoola Abimbola on 3/27/26.
-//
+  //
+  //  TaskBootcamp.swift
+  //  SwiftConcurrencyBootcamp
+  //
+  //  Created by Ayoola Abimbola on 3/27/26.
+  //
 
 import SwiftUI
 
-struct TaskBootcamp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable class TaskBootcampViewModel {
+  var image: UIImage? = nil
+  var image2: UIImage? = nil
+
+  func fetchImage() async {
+    do {
+      guard let url = URL(string: "https://picsum.photos/1000") else { return }
+      let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+
+      await MainActor.run {
+        self.image = UIImage(data: data)
+      }
+
+    } catch {
+      print(error.localizedDescription)
     }
+  }
+
+  func fetchImage2() async {
+    do {
+      guard let url = URL(string: "https://picsum.photos/1000") else { return }
+      let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+
+      self.image2 = UIImage(data: data)
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+
+}
+
+struct TaskBootcamp: View {
+  @State private var viewModel: TaskBootcampViewModel = TaskBootcampViewModel()
+
+  var body: some View {
+    VStack(spacing: 40) {
+      if let image = viewModel.image {
+        Image(uiImage: image)
+          .resizable()
+          .scaledToFit()
+          .frame(width: 200, height: 200)
+      }
+
+      if let image2 = viewModel.image2 {
+        Image(uiImage: image2)
+          .resizable()
+          .scaledToFit()
+          .frame(width: 200, height: 200)
+      }
+    }
+    .onAppear {
+      Task {
+        try? await Task.sleep(nanoseconds: 4000000000)
+        print(Thread.current)
+        print(Task.currentPriority)
+        await viewModel.fetchImage()
+      }
+      Task {
+        await Task.yield()
+        print(Thread.current)
+        print(Task.currentPriority)
+        await viewModel.fetchImage2()
+      }
+    }
+  }
 }
 
 #Preview {
-    TaskBootcamp()
+  TaskBootcamp()
 }
