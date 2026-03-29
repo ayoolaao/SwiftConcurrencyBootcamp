66892d85730df132f7febb0eb714811bcbdfe0e1
Ayoola Abimbola <ao.abimbola@gmail.com>
2026-03-29 03:12:27 -0500

    Task Group init lesson

---
+++ SwiftConcurrencyBootcamp/TaskGroupBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskGroupBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Ayoola Abimbola on 3/29/26.
+//
+
+import SwiftUI
+
+struct TaskGroupBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskGroupBootcamp()
+}
